ae68b79ab294374d0cc9c0b4a2338d97625bad81
Rinor Rexhaj <105825061+RinorRexhaj@users.noreply.github.com>
2024-04-05 12:20:41 +0200

    Test in new branch

--- lab-project.client/src/Products/Products.jsx
+++ lab-project.client/src/Products/Products.jsx
@@ -1,8 +1,21 @@
-
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 const Products = () => {
     return (
-        <h1 className="w-full text-5xl font-semibold flex justify-center items-center">Products</h1>
+        <div className="w-full flex flex-col justify-center items-center">
+            <h1 className="text-5xl font-semibold    flex justify-center items-center">Products</h1>
+            <Card>
+                <CardTitle>Product 1</CardTitle>
+                <CardContent>Product 1 Description</CardContent>
+            </Card>
+        </div>
     ) 
 }
 
