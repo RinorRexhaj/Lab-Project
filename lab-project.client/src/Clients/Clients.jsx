00bb4398f153906e3ffff9a22c927bbb0d635b69
AgonGashii <151534029+AgonGashii@users.noreply.github.com>
2024-05-26 19:23:31 +0200

    Crudi i dyte + lidhja me front
    CRUD i lises se roleve
    Modelet
    Lidhja me front

--- lab-project.client/src/Clients/Clients.jsx
+++ lab-project.client/src/Clients/Clients.jsx
@@ -4,65 +4,171 @@ import axios from "axios";
 const Clients = () => {
     const [clients, setClients] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [newClientData, setNewClientData] = useState({ id: "", fullName: "", email: "" });
+    const [editingClientId, setEditingClientId] = useState(null);
+    const [editing, setEditing] = useState(false);
 
-    // Funksioni për të marrë listën e klientëve nga API
+    useEffect(() => {
+        fetchClients();
+    }, []);
+
     const fetchClients = async () => {
         try {
-            const response = await axios.get("https://localhost:5173/Client"); // Thirrja GET në adresën e API-së për të marrë klientët
-            console.log(response)
-            setClients(response.data); // Përditësoni listën e klientëve me të dhënat e marrura
-            setLoading(false); // Shënjestroni se ngarkimi është kompleto
+            const response = await axios.get("https://localhost:7262/Clients");
+            setClients(response.data);
+            setLoading(false);
         } catch (error) {
             console.error("Error fetching clients:", error);
-            setLoading(false); // Nëse ka ndonjë gabim, shënjestroni se ngarkimi është kompleto
+            setLoading(false);
         }
     };
 
-    // Efekti i ngarkimit të komponentit për të marrë listën e klientëve
-    useEffect(() => {
-        fetchClients();
-    }, []); // Kjo do të thirret vetëm një herë pas ngarkimit të parë të komponentit
-
-    // Funksioni për të fshirë një klient nga lista
     const deleteClient = async (id) => {
         try {
-            await axios.delete(`https://localhost:5173/api/Client/${id}`); // Thirrja DELETE në adresën e API-së për të fshirë klientin
-            setClients(clients.filter(client => client.id !== id)); // Përditësoni listën e klientëve duke hequr klientin e fshirë
+            await axios.delete(`https://localhost:7262/Clients/${id}`);
+            setClients(clients.filter(client => client.id !== id));
         } catch (error) {
             console.error("Error deleting client:", error);
         }
     };
 
-    // Funksioni për të redaktuar një klient
     const editClient = (id) => {
-        // Krijoni logjikën për redaktimin e klientit
-        console.log("Editing client with ID:", id);
+        setEditingClientId(id);
+        const clientToEdit = clients.find(client => client.id === id);
+        setNewClientData(clientToEdit);
+        setEditing(true);
     };
 
-    return (
-        <div>
-            <h1>Lista e Klientëve</h1>
+    const cancelEdit = () => {
+        setEditingClientId(null);
+        setNewClientData({ id: "", fullName: "", email: "" });
+        setEditing(false);
+    };
+
+    const saveClient = async () => {
+        try {
+            if (editingClientId) {
+                await axios.put(`https://localhost:7262/Clients/${newClientData.id}`, newClientData);
+                const updatedClients = clients.map(client =>
+                    client.id === newClientData.id ? newClientData : client
+                );
+                setClients(updatedClients);
+                setEditingClientId(null);
+                setNewClientData({ id: "", fullName: "", email: "" });
+                setEditing(false);
+            } else {
+                const response = await axios.post("https://localhost:7262/Clients", newClientData);
+                setClients([...clients, response.data]);
+                setNewClientData({ id: "", fullName: "", email: "" });
+            }
+        } catch (error) {
+            console.error("Error saving client:", error);
+        }
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewClientData({ ...newClientData, [name]: value });
+    };
+
+    return (        
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Client list</h1>
+            <div className="mb-4">
+                {!editing && ( // Shtojmë këtë pjesë që të shfaqet butoni vetëm kur nuk jemi duke redaktuar një klient
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => setEditing(true)}
+                    >
+                        Add Client
+                    </button>
+                )}
+            </div>
+            {editing && (
+    <div className="mb-4">
+        <input
+            type="text"
+            name="id"
+            value={newClientData.id}
+            onChange={handleInputChange}
+            placeholder="ID"
+            className="border border-gray-400 p-2 mr-2"
+        />
+        <input
+            type="text"
+            name="fullName"
+            value={newClientData.fullName}
+            onChange={handleInputChange}
+            placeholder="Emri"
+            className="border border-gray-400 p-2 mr-2"
+        />
+        <input
+            type="email"
+            name="email"
+            value={newClientData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+            className="border border-gray-400 p-2 mr-2"
+        />
+        <select
+            name="role"
+            value={newClientData.role}
+            onChange={handleInputChange}
+            className="border border-gray-400 p-2 mr-2"
+        >
+            <option value="">Zgjidh Rolin</option>
+            <option value="admin">Admin</option>
+            <option value="user">User</option>
+            <option value="guest">Guest</option>
+        </select>
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={saveClient}
+        >
+            {editingClientId ? "Save" : "Add Client"}
+        </button>
+        <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
+            onClick={cancelEdit}
+        >
+            Cancel
+        </button>
+    </div>
+)}
+
             {loading ? (
                 <p>Loading...</p>
             ) : (
-                <table>
+                <table className="table-auto">
                     <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>Emri</th>
-                            <th>Email</th>
-                            <th>Veprimet</th>
+                            <th className="px-4 py-2">ID</th>
+                            <th className="px-4 py-2">Emri</th>
+                            <th className="px-4 py-2">Email</th>
+                            <th className="px-4 py-2">Roli</th>
+                            <th className="px-4 py-2">Veprimet</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {clients.length >0 && clients.map((client) => (
+                        {clients.map((client) => (
                             <tr key={client.id}>
-                                <td>{client.id}</td>
-                                <td>{client.fullName}</td>
-                                <td>{client.email}</td>
-                                <td>
-                                    <button onClick={() => editClient(client.id)}>Edit</button>
-                                    <button onClick={() => deleteClient(client.id)}>Delete</button>
+                                <td className="border px-20 py-2">{client.id}</td>
+                                <td className="border px-20 py-2">{client.fullName}</td>
+                                <td className="border px-20 py-2">{client.email}</td>
+                                <td className="border px-15 py-2">{client.role}</td> {/* Shto këtë rresht për të shfaqur rolin */}
+                                <td className="border px-20 py-2">
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                                        onClick={() => editClient(client.id)}
+                                    >
+                                        Edit
+                                    </button>
+                                    <button
+                                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                        onClick={() => deleteClient(client.id)}
+                                    >
+                                        Delete
+                                    </button>
                                 </td>
                             </tr>
                         ))}
--- lab-project.client/src/components/Modal.jsx
+++ lab-project.client/src/components/Modal.jsx
@@ -93,144 +93,144 @@ const Modal = ({action, modalVisible, closeModal, elements, setElements, postDat
 
 
     return (
-        <div className={`fixed inset-0 flex items-center justify-center ${modalVisible ? 'opacity-100 z-50': 'opacity-0 -z-99'} transition-opacity duration-200 ease-in`}>
-          <div className="bg-white p-8 rounded-md z-50 w-100 md:w-80">
-            <div className="relative items-center gap-6">
-              <h2 className="relative text-3xl md:text-2xl mb-8 font-bold text-slate-800">
-                {!actionDone ? `${action[0] + action.slice(1).toLowerCase()} Your Product` : `Your product was ${action !== "DELETE" ? action[0]+action.slice(1).toLowerCase()+"ed" : action[0]+action.slice(1).toLowerCase()+"d"}`}
-              </h2>
-              {actionDone && <FontAwesomeIcon className="w-7 h-7 m-auto text-green-500 animate-ping-once"  icon={faCircleCheck} />}
-              <button
-                className="absolute -top-6 -right-4 text-4xl"
-                onClick={() => {
-                    setEditData({
-                        id: "",
-                        name: "",
-                        category: "",
-                        price: "",
-                        image: ""
-                      });
-                    closeModal();
-                }}
-              >
-                &times;
-              </button>
-            </div>
-
-            {!actionDone && <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-              {action !== "DELETE" && <>
-              <div className="relative flex items-center justify-between ">
-                <label
-                  htmlFor="id"
-                  className="text-lg font-bold text-slate-600"
-                >
-                  ID
-                </label>
-                <input
-                  type="text"
-                  className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
-                  id="id"
-                  placeholder="Product ID..."
-                  defaultValue={action === "EDIT" ? editData.id : ""}
-                  readOnly={action === "EDIT" ? true : false}
-                  onChange={(e) => {
-                    handleErrors(e);
-                    if(action === "POST") setPostData({...postData, id: e.target.value});
-                    else if(action === "EDIT") setEditData({...editData, id: e.target.value});
-                  }}
-                  required />
-                  <div className="h-1 absolute bottom-0 left-25 text-red-500 font-medium text-md">{idError}</div>
-              </div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="name"
-                  className=" text-lg font-bold text-slate-600"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
-                  id="name"
-                  placeholder="Product name..."
-                  defaultValue={action === "EDIT" ? editData.name : ""}
-                  onChange={e => {
-                    handleErrors(e);
-                    if(action === "POST") setPostData({...postData, name: e.target.value});
-                    else if(action === "EDIT") setEditData({...editData, name: e.target.value});
-                  }}
-                  required 
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="category"
-                  className="text-lg font-bold text-slate-600"
-                >
-                  Category
-                </label>
-                <input
-                  type="text"
-                  className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
-                  id="category"
-                  placeholder="Product Category..."
-                  defaultValue={editData.category}
-                  onChange={e => {
-                    handleErrors(e);
-                    if(action === "POST") setPostData({...postData, category: e.target.value});
-                    else if(action === "EDIT") setEditData({...editData, category: e.target.value});
-                  }}
-                  required 
-                />
-              </div>
-              <div className="relative flex items-center justify-between">
-                <label
-                  htmlFor="price"
-                  className="text-lg font-bold text-slate-600"
-                >
-                  Price
-                </label>
-                <input
-                  type="text"
-                  className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
-                  id="price"
-                  placeholder="Product Price..."
-                  defaultValue={editData.price}
-                  onChange={e => {
-                    handleErrors(e);
-                    if(action === "POST") setPostData({...postData, price: e.target.value});
-                    else if(action === "EDIT") setEditData({...editData, price: e.target.value});
-                  }}
-                  required 
-                />
-                <div className="h-1 absolute bottom-0 left-25 text-red-500 font-medium text-md">{priceError}</div>
-              </div>
-              <div className="flex items-center justify-between">
-                <label className=" text-lg font-bold text-slate-600">
-                  Image
-                </label>
-                <input
-                  type="file"
-                  accept="image/png, image/jpg, image/jpeg"
-                  className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
-                  onChange={e => {
-                    setProductImage(e.target.files[0]);
-                  }}
-                />
-              </div>
-              </>}
-              {action === "DELETE" && 
-                <h1 className="text-xl font-medium text-center">Are you sure you want to delete product "{deleteId}" ?</h1>
-              }
-
-              <button className={`w-35 md:w-25 h-10 flex items-center justify-center gap-2 rounded-md font-medium ${action !== "DELETE" ? "bg-blue-600 hover:bg-blue-500" : "bg-red-600 hover:bg-red-500"} duration-150 ease-linear text-white ml-25`} type="submit">
-                <FontAwesomeIcon icon={action === "POST" ? faPlus : action === "EDIT" ? faPenToSquare : faTrash} /> {action}{" "}
-              </button>
-            </form>}
+      <div className={`fixed inset-0 flex items-center justify-center ${modalVisible ? 'opacity-100 z-50': 'opacity-0 -z-99'} transition-opacity duration-200 ease-in`}>
+        <div className="bg-white p-8 rounded-md z-50 w-100 md:w-80">
+          <div className="relative items-center gap-6">
+            <h2 className="relative text-3xl md:text-2xl mb-8 font-bold text-slate-800">
+              {!actionDone ? `${action[0] + action.slice(1).toLowerCase()} Your Product` : `Your product was ${action !== "DELETE" ? action[0]+action.slice(1).toLowerCase()+"ed" : action[0]+action.slice(1).toLowerCase()+"d"}`}
+            </h2>
+            {actionDone && <FontAwesomeIcon className="w-7 h-7 m-auto text-green-500 animate-ping-once"  icon={faCircleCheck} />}
+            <button
+              className="absolute -top-6 -right-4 text-4xl"
+              onClick={() => {
+                  setEditData({
+                      id: "",
+                      name: "",
+                      category: "",
+                      price: "",
+                      image: ""
+                    });
+                  closeModal();
+              }}
+            >
+              &times;
+            </button>
           </div>
-          <div className="overlay fixed inset-0 bg-black opacity-50"></div>
+
+          {!actionDone && <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+            {action !== "DELETE" && <>
+            <div className="relative flex items-center justify-between ">
+              <label
+                htmlFor="id"
+                className="text-lg font-bold text-slate-600"
+              >
+                ID
+              </label>
+              <input
+                type="text"
+                className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
+                id="id"
+                placeholder="Product ID..."
+                defaultValue={action === "EDIT" ? editData.id : ""}
+                readOnly={action === "EDIT" ? true : false}
+                onChange={(e) => {
+                  handleErrors(e);
+                  if(action === "POST") setPostData({...postData, id: e.target.value});
+                  else if(action === "EDIT") setEditData({...editData, id: e.target.value});
+                }}
+                required />
+                <div className="h-1 absolute bottom-0 left-25 text-red-500 font-medium text-md">{idError}</div>
+            </div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="name"
+                className=" text-lg font-bold text-slate-600"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
+                id="name"
+                placeholder="Product name..."
+                defaultValue={action === "EDIT" ? editData.name : ""}
+                onChange={e => {
+                  handleErrors(e);
+                  if(action === "POST") setPostData({...postData, name: e.target.value});
+                  else if(action === "EDIT") setEditData({...editData, name: e.target.value});
+                }}
+                required 
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="category"
+                className="text-lg font-bold text-slate-600"
+              >
+                Category
+              </label>
+              <input
+                type="text"
+                className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
+                id="category"
+                placeholder="Product Category..."
+                defaultValue={editData.category}
+                onChange={e => {
+                  handleErrors(e);
+                  if(action === "POST") setPostData({...postData, category: e.target.value});
+                  else if(action === "EDIT") setEditData({...editData, category: e.target.value});
+                }}
+                required 
+              />
+            </div>
+            <div className="relative flex items-center justify-between">
+              <label
+                htmlFor="price"
+                className="text-lg font-bold text-slate-600"
+              >
+                Price
+              </label>
+              <input
+                type="text"
+                className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
+                id="price"
+                placeholder="Product Price..."
+                defaultValue={editData.price}
+                onChange={e => {
+                  handleErrors(e);
+                  if(action === "POST") setPostData({...postData, price: e.target.value});
+                  else if(action === "EDIT") setEditData({...editData, price: e.target.value});
+                }}
+                required 
+              />
+              <div className="h-1 absolute bottom-0 left-25 text-red-500 font-medium text-md">{priceError}</div>
+            </div>
+            <div className="flex items-center justify-between">
+              <label className=" text-lg font-bold text-slate-600">
+                Image
+              </label>
+              <input
+                type="file"
+                accept="image/png, image/jpg, image/jpeg"
+                className="w-60 md:w-40 appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:border-blue-500 focus:shadow-outline bg-blue-50 border  border-slate-200"
+                onChange={e => {
+                  setProductImage(e.target.files[0]);
+                }}
+              />
+            </div>
+            </>}
+            {action === "DELETE" && 
+              <h1 className="text-xl font-medium text-center">Are you sure you want to delete product "{deleteId}" ?</h1>
+            }
+
+            <button className={`w-35 md:w-25 h-10 flex items-center justify-center gap-2 rounded-md font-medium ${action !== "DELETE" ? "bg-blue-600 hover:bg-blue-500" : "bg-red-600 hover:bg-red-500"} duration-150 ease-linear text-white ml-25`} type="submit">
+              <FontAwesomeIcon icon={action === "POST" ? faPlus : action === "EDIT" ? faPenToSquare : faTrash} /> {action}{" "}
+            </button>
+          </form>}
         </div>
-    );
+        <div className="overlay fixed inset-0 bg-black opacity-50"></div>
+      </div>
+  );
 }
 
 export default Modal;
